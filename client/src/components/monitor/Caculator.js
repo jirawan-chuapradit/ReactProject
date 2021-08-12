7262444872bbe9819a8781e406027a7e730a074e
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2021-08-12 16:08:39 +0700

    การส่งข้อมูลผ่าน API ด้วย HTTP POST

--- client/src/components/monitor/Caculator.js
+++ client/src/components/monitor/Caculator.js
@@ -1,40 +1,52 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
 
 class Calculator extends Component {
-
-    showOrder(orders) {
-        if (!orders || orders.length == 0) {
-            return <li className="text-end text-muted"> ไม่มีสินค้า </li>
-        }else {
-            return orders.map(order => {
-                return (
-                    <li className="text-end text-success  title" key={order.product.productId}>
-                        {order.product.productName} x {order.quantity} = {order.product.unitPrice * order.quantity}
-                        <button className="btn btn-light btn-sm" onClick={() => this.props.onDelOrder(order.product)}>X</button>
-                    </li>
-                )
-            })
-        }
-    }
-    render() {
-        const {totalPrice, orders} = this.props
+  showOrder(orders) {
+    if (!orders || orders.length == 0) {
+      return <li className="text-end text-muted"> ไม่มีสินค้า </li>;
+    } else {
+      return orders.map((order) => {
         return (
-            <div>
-                <h1 className="text-end">{totalPrice}</h1>
-                <hr />
-
-                <ul className="list-unstyled">
-                    {this.showOrder(orders)}               
-                </ul>
-                <hr />
-                <div className="d-grid gap-2">
-                    <button className="btn btn-danger" type="button">ยืนยัน</button>
-                    <button className="btn btn-secondary" type="button">ยกเลิก</button>
-                </div>
-            </div>
-        )
+          <li
+            className="text-end text-success  title"
+            key={order.product.productId}
+          >
+            {order.product.productName} x {order.quantity} ={" "}
+            {order.product.unitPrice * order.quantity}
+            <button
+              className="btn btn-light btn-sm"
+              onClick={() => this.props.onDelOrder(order.product)}
+            >
+              X
+            </button>
+          </li>
+        );
+      });
     }
+  }
+  render() {
+    const { totalPrice, orders } = this.props;
+    return (
+      <div>
+        <h1 className="text-end">{totalPrice}</h1>
+        <hr />
+
+        <ul className="list-unstyled">{this.showOrder(orders)}</ul>
+        <hr />
+        <div className="d-grid gap-2">
+          <button className="btn btn-danger" onClick={() => this.props.onConfirmOrder()} >
+            ยืนยัน
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => this.props.onCancelOrder()}
+          >
+            ยกเลิก
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
-
-export default Calculator
+export default Calculator;
--- client/src/components/monitor/Monitor.js
+++ client/src/components/monitor/Monitor.js
@@ -1,50 +1,85 @@
-import React, {Component} from "react"
-import Calculator from "./Caculator"
-import ProductList from "../product/ProductList"
+import React, { Component } from "react";
+import Calculator from "./Caculator";
+import ProductList from "../product/ProductList";
+import axios from "axios";
 
 class Monitor extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { totalPrice: 0, orders: [] };
+    this.addOrder = this.addOrder.bind(this);
+    this.delOrder = this.delOrder.bind(this);
+    this.cancelOrder = this.cancelOrder.bind(this);
+    this.confirmOrder = this.confirmOrder.bind(this);
+  }
 
-    constructor (props) {
-        super(props)
-        this.state = {totalPrice: 0, orders:[]}
-        this.addOrder = this.addOrder.bind(this)
-        this.delOrder = this.delOrder.bind(this)
+  addOrder(product) {
+    let findOrder = this.state.orders.find(
+      (order) => order.product.productId === product.productId
+    );
+    if (findOrder) {
+      findOrder.quantity++;
+    } else {
+      this.state.orders.push({ product: product, quantity: 1 });
     }
+    const totalPrice = this.state.totalPrice + parseInt(product.unitPrice);
+    this.setState({ totalPrice: totalPrice, orders: this.state.orders });
+  }
 
-    addOrder(product) {
-        let findOrder = this.state.orders.find(order => order.product.productId == product.productId)
-        if (findOrder) {
-            findOrder.quantity++
-        }else {
-            this.state.orders.push({product: product, quantity: 1})
-        }
-        const totalPrice = this.state.totalPrice + parseInt(product.unitPrice)
-        this.setState({totalPrice: totalPrice, orders: this.state.orders})
-    }
+  delOrder(product) {
+    let findOrder = this.state.orders.find(
+      (order) => order.product.productId === product.productId
+    );
+    let resultOrders = this.state.orders.filter(
+      (order) => order.product.productId !== product.productId
+    );
+    const totalPrice =
+      this.state.totalPrice -
+      findOrder.quantity * parseInt(findOrder.product.unitPrice);
+    this.setState({ totalPrice: totalPrice, orders: resultOrders });
+  }
 
-    delOrder(product) {
-        let findOrder = this.state.orders.find(order => order.product.productId == product.productId)
-        let resultOrders = this.state.orders.filter(order => order.product.productId != product.productId)
-        const totalPrice = this.state.totalPrice - findOrder.quantity * parseInt(findOrder.product.unitPrice)
-        this.setState({totalPrice: totalPrice, orders:resultOrders})
-    }
+  confirmOrder() {
+    const { totalPrice, orders } = this.state;
+    axios
+      .post("http://localhost:3001/orders", {
+        orderDate: new Date(),
+        totalPrice,
+        orders,
+      })
+      .then((res) => {
+        this.setState({ totalPrice: 0, orders: [] });
+      });
+  }
 
-    render() {
-        return (
-            <div className="container-fluid">
-                <div className="row"> 
-                    <div className="col-md-9">
-                        <ProductList products={this.props.products} onAddOrder={this.addOrder} />
-                    </div>
+  cancelOrder() {
+    this.setState({ totalPrice: 0, orders: [] });
+  }
 
-                    <div className="col-md-3">
-                        <Calculator totalPrice={this.state.totalPrice} orders={this.state.orders} onDelOrder={this.delOrder}/>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-9">
+            <ProductList
+              products={this.props.products}
+              onAddOrder={this.addOrder}
+            />
+          </div>
 
-            </div>
-        )
-    }
+          <div className="col-md-3">
+            <Calculator
+              totalPrice={this.state.totalPrice}
+              orders={this.state.orders}
+              onDelOrder={this.delOrder}
+              onConfirmOrder={this.confirmOrder}
+              onCancelOrder={this.cancelOrder}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Monitor
+export default Monitor;
