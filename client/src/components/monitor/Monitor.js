99aa75050c47eb2cc206a65fd91fda73701af9d1
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2021-08-15 01:34:42 +0700

    การแจ้งเตือน

--- client/src/components/monitor/Monitor.js
+++ client/src/components/monitor/Monitor.js
@@ -6,7 +6,7 @@ import axios from "axios";
 class Monitor extends Component {
   constructor(props) {
     super(props);
-    this.state = { totalPrice: 0, orders: [] };
+    this.state = { totalPrice: 0, orders: [], confirm: false, msg: `` };
     this.addOrder = this.addOrder.bind(this);
     this.delOrder = this.delOrder.bind(this);
     this.cancelOrder = this.cancelOrder.bind(this);
@@ -23,7 +23,11 @@ class Monitor extends Component {
       this.state.orders.push({ product: product, quantity: 1 });
     }
     const totalPrice = this.state.totalPrice + parseInt(product.unitPrice);
-    this.setState({ totalPrice: totalPrice, orders: this.state.orders });
+    this.setState({
+      totalPrice: totalPrice,
+      orders: this.state.orders,
+      confirm: false,
+    });
   }
 
   delOrder(product) {
@@ -36,29 +40,53 @@ class Monitor extends Component {
     const totalPrice =
       this.state.totalPrice -
       findOrder.quantity * parseInt(findOrder.product.unitPrice);
-    this.setState({ totalPrice: totalPrice, orders: resultOrders });
+    this.setState({
+      totalPrice: totalPrice,
+      orders: resultOrders,
+      confirm: false,
+    });
   }
 
   confirmOrder() {
     const { totalPrice, orders } = this.state;
-    axios
-      .post("http://localhost:3001/orders", {
-        orderDate: new Date(),
-        totalPrice,
-        orders,
-      })
-      .then((res) => {
-        this.setState({ totalPrice: 0, orders: [] });
+    if (orders && orders.length > 0) {
+      axios
+        .post("http://localhost:3001/orders", {
+          orderDate: new Date(),
+          totalPrice,
+          orders,
+        })
+        .then((res) => {
+          this.setState({
+            totalPrice: 0,
+            orders: [],
+            confirm: true,
+            msg: `บันทึกรายการสั่งซื้อสำเร็จ`,
+          });
+        });
+    } else {
+      this.setState({
+        totalPrice: 0,
+        orders: [],
+        confirm: true,
+        msg: `เลือกสินค้าก่อนค่ะ`,
       });
+    }
   }
 
   cancelOrder() {
-    this.setState({ totalPrice: 0, orders: [] });
+    this.setState({ totalPrice: 0, orders: [], confirm: false });
   }
 
   render() {
     return (
       <div className="container-fluid">
+        {this.state.confirm && (
+          <div className="alert alert-secondary text-end" role="alert">
+            {this.state.msg}
+          </div>
+        )}
+
         <div className="row">
           <div className="col-md-9">
             <ProductList
