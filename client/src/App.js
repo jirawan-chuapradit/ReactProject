8a30b069fa4f0c8b6eb4ffda0f14accb731eeaf1
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2021-08-22 14:29:20 +0700

    add order page

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import About from "./containers/About";
+import Order from "./containers/order/Order";
 class App extends Component {
   renderRouter() {
     return (
       <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
+        <Route path="/orders" component={Order} />
       </Switch>
     );
   }
