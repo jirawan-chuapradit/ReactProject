d0e1998bbd727f66b98b24e8be5b7e56291c0bcc
Jirawan.C <jirawan-chuapradit@users.noreply.github.com>
2021-08-25 13:13:52 +0700

    create menu bar with Link

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,47 +1,78 @@
-import React,{Component} from "react"
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {date : new Date()}
-        
-        console.log('constructor')
-    }
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
 
-    componentDidMount(){
-        this.timerID = setInterval(() => {
-            this.tick()
-        }, 1000);
-        console.log('componentDidMount')
-    }
+    console.log("constructor");
+  }
 
-    componentDidUpdate(){
-        // console.log('componentDidUpdate')
-    }
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.tick();
+    }, 1000);
+    console.log("componentDidMount");
+  }
 
-    componentWillUnmount(){
-    clearInterval(this.timerID)        
-    }
+  componentDidUpdate() {
+    // console.log('componentDidUpdate')
+  }
 
-    tick() {
-        this.setState({date : new Date()})
-    }
-    
-    render() {
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
 
-        return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-md-8 text-left">
-                    <h1 className="text-success"><img style={{height : 70}} src="/images/logo/logo.png" atl="" /> เฮลตี้ คาเฟ่ </h1>
-                </div>
-                <div className="col-md-4 text-end">
-                    <h5 className="text-muted mt-4"> {this.state.date.toLocaleTimeString()} </h5>
-                </div>
-                <hr />
-            </div>
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-8 text-left">
+            <h1 className="text-success">
+              <img style={{ height: 70 }} src="/images/logo/logo.png" atl="" />{" "}
+              เฮลตี้ คาเฟ่{" "}
+            </h1>
+          </div>
+          <div className="col-md-4 text-end">
+            <h5 className="text-muted mt-4">
+              {" "}
+              {this.state.date.toLocaleTimeString()}{" "}
+            </h5>
+            <ul className="list-inline">
+              <li className="list-inline-item">
+                <Link to="/" className="text-success">
+                  หน้าหลัก
+                </Link>
+              </li>
+              <li className="list-inline-item">|</li>
+              <li className="list-inline-item">
+                <Link to="/orders" className="text-success">
+                  รายการสั่งซื้อ
+                </Link>
+              </li>
+              <li className="list-inline-item">|</li>
+              <li className="list-inline-item">
+                <Link to="/products" className="text-success">
+                  สินค้า
+                </Link>
+              </li>
+              <li className="list-inline-item">|</li>
+              <li className="list-inline-item">
+                <Link to="/about" className="text-success">
+                  เกี่ยวกับเรา
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <hr />
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 export default Header;
